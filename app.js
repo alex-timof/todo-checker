867e48e16aae699882bdade577b285d9d584d8f3
Great-Shurick <thegrtshurik@gmail.com>
2019-04-10 23:57:35 +0300

    Try electron.js
    Just playing with electronJS

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const url = require('url');
+const {app, BrowserWindow} = require('electron');
+
+let win;
+
+function createWindow(){
+  win = new BrowserWindow(
+    {
+      width: 700,
+      height: 500,
+      icon: __dirname + "/_img/icon.png",
+    });
+
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true,
+    }));
+find()
+    //win.webContents.openDevTools();
+
+
+    win.on('closed', ()=>{
+
+      win = null;
+
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', ()=>{
+  app.quit();
+})
