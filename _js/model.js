7cc26def85b04728e0c43bc0863d52f7280ee678
Great-Shurick <thegrtshurik@gmail.com>
2019-04-14 18:48:09 +0300

    Lot of changes
    Added algorithn for folder analysis. Added input form for choosing path

--- _js/model.js
+++ _js/model.js
@@ -1,18 +1,31 @@
-todos = [
-  { text: 'Learn JavaScript', value: '3', time: "None" , n: "false"},
-  { text: 'Learn Vue', value: '1', time: "None", n: "false" },
-  { text: 'Build something awesome', value: '4', time: "None", n: "false" },
-]
+coreFolder = ""
+// TODO: task in model.js
+const fs = require('fs');
+var todos = [];
+var test = ""
+const re = new RegExp('.*todo?:\s*([^\n\r]*)','i');
 
+function updateTasks(pathName = coreFolder+'\\'){
+  var files = fs.readdirSync(pathName);
+  files.forEach((fileName)=>{
+    try {
+        var fileText = fs.readFileSync(pathName+fileName,'utf8')
+        var sentence = fileText.split("\n");
+        sentence.forEach((value)=>{
+          search = value.match(re);
+          if(search!==null){
+            createTask(search[1], fs.statSync(pathName+fileName)['birthtime'], pathName+fileName);
+          }
+        });
+    }catch(err) {
+      if(fileName!=='node_modules' && fileName!=='.git'){
+        updateTasks(pathName+fileName+"\\");
+      }
+    }
+  });
 
-createTask("test")
-createTask("test")
-createTask("test1","false")
-createTask("test")
-createTask("test")
+}
 
-
-function createTask(text, n = "true", date = new Date(), value=null){
-  todos.push({text: text, value: null, time:date, n: n});
-  //setNewTag(text);
+function createTask(text, date, path){
+  todos.push({text: text, time:date, path:path});
 }
--- _js/vue.js
+++ _js/vue.js
@@ -1,6 +1,31 @@
 new Vue({
-  el: '#app',
+  el: '#tasks',
   data:{
     todos: todos
+  },
+  methods: {
+    details: function (t) {
+      alert(t.text+"\n"+t.path+"\n"+t.time);
+    }
+  }
+});
+// TODO: task in vue.js
+new Vue({
+  el: '#path',
+  methods: {
+    update: function () {
+      d = document.getElementById('text');
+      t = this.$refs.text.value;
+      x = ''
+      for (var i = 0; i < t.length; i++) {
+        ch = t.charAt(i);
+        if(ch=='\\')
+          x +='\\';
+        x +=ch;
+        if(i==t.length-1 && ch!=="\\")
+          x +='\\';
+      }
+      updateTasks(x);
+    }
   }
 });
